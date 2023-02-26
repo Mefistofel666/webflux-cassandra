12575dd66264dd2ad8d6fe1c182ef77104df7a29
Kirill Duvakin <kbduvakin1@mts.ru>
2023-02-26 21:43:56 +0300

    added spring-rest-reactive

---
+++ spring-rest-reactive/src/main/java/org/smarttechie/controller/ProductController.java
@@ -0,0 +1,60 @@
+package org.smarttechie.controller;
+
+import org.smarttechie.model.Product;
+import org.smarttechie.repository.ProductRepository;
+import org.smarttechie.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/api")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    /**
+     * This endpoint allows to create a product.
+     * @param product - to create
+     * @return - the created product
+     */
+    @PostMapping("/product")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<Product> createProduct(@RequestBody Product product){
+        return productService.save(product);
+    }
+
+    /**
+     * This endpoint gives all the products
+     * @return - the list of products available
+     */
+    @GetMapping("/products")
+    public Flux<Product> getAllProducts(){
+        return productService.getAllProducts();
+    }
+
+    /**
+     * This endpoint allows to delete a product
+     * @param id - to delete
+     * @return
+     */
+    @DeleteMapping("/product/{id}")
+    public Mono<Void> deleteProduct(@PathVariable int id){
+        return productService.deleteProduct(id);
+    }
+
+    /**
+     * This endpoint allows to update a product
+     * @param product - to update
+     * @return - the updated product
+     */
+    @PutMapping("product/{id}")
+    public Mono<ResponseEntity<Product>> updateProduct(@RequestBody Product product){
+        return productService.update(product);
+    }
+}
